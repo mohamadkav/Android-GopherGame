ff7e95801b6f3dff6ea0f2a05723f6b845129399
Mohammad <mohamad_kav@yahoo.co.uk>
2016-04-02 14:51:35 +0430

    First commit

---
+++ app/src/main/java/io/sharif/prj1/st91110439/st91106019/project1/MainActivity.java
@@ -0,0 +1,20 @@
+package io.sharif.prj1.st91110439.st91106019.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
